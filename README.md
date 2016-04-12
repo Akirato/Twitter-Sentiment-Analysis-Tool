348c478db6199fbeee4d75644b735adbcd25750d
Akirato <nurendra.choudhary@research.iiit.ac.in>
2016-04-12 18:35:40 +0530

    Added the web2py module too

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 A Sentiment Analysis for Twitter Data.
 
 #Working:
-- Run: python get_sentiment_bigram.py
+- model.en is generated using word2vec using make_word2vec_model.py
+- vectorize using vectorize.py
+- generate random_for_predictor.pickle using train_and_validate.py
+- Run: python get_sentiment_word.py
+##(For Detailed)
+- Install the "sentimentAnalysis" in your local web2py at "127.0.0.1:8000"
 
-##Features built till now:
-- Emoticon Scoring
-- Predefined Scores for words
-- Twitter Slang Abbreviation Handling
+##Support:
+- WebPage at http://akirato.github.io/Twitter-Sentiment-Analysis-Tool/
+- Video with Demo and Accuracies: https://youtu.be/VuR16P87yPE
+- For any doubts, Raise an Issue and we will sort it out.
+
 
---
+++ sentimentAnalysis/controllers/default.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+def show():
+    li = request.args
+    return dict(sentence = li[0].replace('_',' '),result1 = li[1].upper(),result2 = li[2],result3 = li[3],result4 = li[4],result5 = li[5])
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
